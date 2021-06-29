46406463a4bbcf4ecec5d58962c7bccc1115fcee
Linh Hoang <Linh.Hoang@devmounta.in>
2021-06-29 12:59:35 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+const port = process.env.PORT || 4545
+app.listen(port, ()=>{console.log(`Server jamming on ${port}`)})
