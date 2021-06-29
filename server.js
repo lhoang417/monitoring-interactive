e5379474912daaced535566c3121f15e9f830b75
Linh Hoang <Linh.Hoang@devmounta.in>
2021-06-29 13:30:23 -0500

    added APM

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '929e2ddedf0c4004a3e8a4b3adc9ffce',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 app.use(express.json());
 
 
+
 app.get('/', (req,res)=>{
+    rollbar.log('Hello world!')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
